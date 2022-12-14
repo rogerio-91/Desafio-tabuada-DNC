7e2a39f2bde5c4b522d0952cf627c37defee4a59
rogerio-91 <118301838+rogerio-91@users.noreply.github.com>
2022-12-13 21:52:36 -0300

    Projeto de tabuada realizado

---
+++ tabuada_script.js
@@ -0,0 +1,23 @@
+function Tabuada(){
+    var num = window.document.getElementById('num')
+    var end = window.document.getElementById('end')
+    if (num.value.lenght == null || end.value.lenght == null){
+        window.alert("[ERRO] Confira os dados!")
+    }else{
+
+    
+        var n = Number(num.value)
+        var e = Number(end.value)
+        var x = 0
+        var r = ''
+        var res = window.document.getElementById('resposta')
+        res.innerHTML = `A tabuada do ${n} é:<br>`
+        do{
+            r = n*x
+            res.innerHTML += `${x} X ${n} = ${r} <br>`
+            x++
+        }while (x <= e)
+    }
+}    
+
+Tabuada ()
